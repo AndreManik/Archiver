acdd28132e1806f3330fabe5bf7f3be65102016e
AndreManik <andre.ivan.manik@gmail.com>
2017-06-12 17:21:52 +0300

    Add files via upload

--- command/Command.java
+++ command/Command.java
@@ -1,8 +1,5 @@
 package com.javarush.task.task31.task3110.command;
 
-/**
- * Created by Andre on 12.06.2017.
- */
 public interface Command {
     void execute() throws Exception;
 }
---
+++ command/ZipCommand.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task31.task3110.command;
+
+/**
+ * Created by Andre on 12.06.2017.
+ */
+public abstract class ZipCommand implements Command {
+}
