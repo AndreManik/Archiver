eae03d904a30d574c2c250a938c0c224f8bdc922
AndreManik <andre.ivan.manik@gmail.com>
2017-06-12 20:24:33 +0300

    Add files via upload

---
+++ exception/PathIsNotFoundException.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task31.task3110.exception;
+
+/**
+ * Created by Andre on 12.06.2017.
+ */
+public class PathIsNotFoundException extends Exception {
+}
---
+++ exception/WrongZipFileException.java
@@ -0,0 +1,8 @@
+package com.javarush.task.task31.task3110.exception;
+
+/**
+ * Created by Andre on 12.06.2017.
+ */
+public class WrongZipFileException extends Exception {
+
+}
