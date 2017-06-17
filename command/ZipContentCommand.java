5f0080eac457404e462f5e7e064ea13f5eca0852
AndreManik <andre.ivan.manik@gmail.com>
2017-06-17 12:15:39 +0300

    Add files via upload

--- command/ZipContentCommand.java
+++ command/ZipContentCommand.java
@@ -1,11 +1,24 @@
 package com.javarush.task.task31.task3110.command;
 
+import com.javarush.task.task31.task3110.ConsoleHelper;
+import com.javarush.task.task31.task3110.FileProperties;
+import com.javarush.task.task31.task3110.ZipFileManager;
+
+import java.util.List;
+
 /**
  * Created by Andre on 12.06.2017.
  */
 public class ZipContentCommand extends ZipCommand {
     @Override
     public void execute() throws Exception {
+        ConsoleHelper.writeMessage("Содержимое архива:");
+        ZipFileManager zipFileManager = getZipFileManager();
 
+        List<FileProperties> list = zipFileManager.getFilesList();
+
+        for(FileProperties fp : list){
+            ConsoleHelper.writeMessage(fp.toString());
+        }
     }
 }
